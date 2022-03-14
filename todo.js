cd27d08ffc1251069c83951ae3a0c472f3ca1326
Testas <testas@testas.lt>
2022-03-14 15:16:47 +0200

    1

--- todo.js
+++ todo.js
@@ -21,29 +21,39 @@
 }());
 
 
-
+/////////////////////////////////////////////////   Modal ADD kodas
 // Get the modal
-var modal = document.getElementById("myModalAdd");
+var modalAdd = document.getElementById("myModalAdd");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtnAdd");
+var btnAdd = document.getElementById("myBtnAdd");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-  modal.style.display = "block";
+  modalAdd.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+  modalAdd.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalAdd) {
+    modalAdd.style.display = "none";
   }
 }
+
+///////////////////////////////////////////////  Delete kortele kodas
+
+function myKorteleDelete(myKortID) {
+    let isExecuted = confirm("Ar tikrai triti kortele?");
+        if (isExecuted) {   
+            const myobj = document.getElementById(myKortID);
+            myobj.remove();
+        }    
+  }
